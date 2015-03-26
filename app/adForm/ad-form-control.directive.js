02aa380ff1ef4038d17da63a20fc68094bc50e4c
Dieter <dieter.goetelen@hotmail.com>
2015-03-26 20:23:39 +0100

    added attributes

---
+++ app/adForm/ad-form-control.directive.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+
+
+    angular.module('acaddemict.form')
+        .directive('adFormControl', adForm);
+
+    adForm.$inject = [];
+    function adForm() {
+        var vm = {};
+
+        vm.restrict = "E";
+        vm.templateUrl = "app/adForm/ad-form-control.tmpl.html";
+        vm.scope = {
+            text: '@',
+            model: '=',
+            attributes: '='
+        };
+        vm.link = link;
+        vm.controller = controller;
+        vm.controllerAs = "adForm";
+        vm.bindToController = true;
+
+        return vm;
+
+        function controller() {
+
+        }
+
+        function link(scope, element, attrs) {
+            var $element = angular.element(element.find('input')[0]);
+
+            if (angular.isObject(scope.adForm.attributes)) {
+                angular.forEach(scope.adForm.attributes, function (val, key) {
+                    $element.attr(key, val);
+                });
+            }
+        }
+    }
+
+}());
---
+++ app/layout/layout.controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('LayoutController', LayoutController);
+
+    function LayoutController() {
+        var vm = this;
+
+        vm.formData = {};
+    }
+
+}());
